6a054bc7a1abf236c2f881629f110c4206a2d0cd
Manish Suyal <manishsuyal2099@Manishs-MacBook-Air.local>
2024-01-08 01:25:24 +0530

    note list component added with styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dce5e3a48c3f39f313da4a39e88a117ba9c64d0d9d2ebe3189de70c495a9831
-size 161
+oid sha256:a9878db2eed16c9d5e3e0bba919aed3170b64a2fa26c23e250d88faf07b73702
+size 439
--- src/Components/NoteLists.js
+++ src/Components/NoteLists.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36d3d91f06132ff5072d7c16d16afb31ddc882e0d893fff17c1181be8d4880fa
-size 290
+oid sha256:04ca60b485226399e93b83d926cdeccb9c7a71943a00b87bd1e36678a464406b
+size 185
